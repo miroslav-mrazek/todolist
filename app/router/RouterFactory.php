08e1f3e8a01081d1d4d3743d84ce76843c8f4710
Miroslav Mrázek <miroslav.mrazek@gmail.com>
2014-07-21 17:19:38 +0200

    Zkratky pro routy ApplicationPresenteru.

--- app/router/RouterFactory.php
+++ app/router/RouterFactory.php
@@ -12,14 +12,23 @@ class RouterFactory
 	
 	const DEFAULT_ROUTE = 'Catalog:list';
 
+	
 	/**
 	 * @return IRouter
 	 */
 	public function createRouter()
 	{
 		$router = new RouteList();
+		
+		# jednosměrná routa pro zpracování přímého volání souboru index.php
 		$router[] = new Route('index.php', self::DEFAULT_ROUTE, Route::ONE_WAY);
+		
+		# zkratky
+		$router[] = new Route('login', 'Application:login');
+		$router[] = new Route('logout', 'Application:logout');
+		
 		$router[] = new Route('<presenter>/<action>[/<id>]', self::DEFAULT_ROUTE);
+		
 		return $router;
 	}
 
