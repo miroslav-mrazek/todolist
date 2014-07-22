dbff5a3e3decae9ece5f195822792d262956baef
Miroslav Mrázek <miroslav.mrazek@gmail.com>
2014-07-22 03:15:30 +0200

    Změna pravidel dohledávání šablon.

--- app/components/BaseControl.php
+++ app/components/BaseControl.php
@@ -8,4 +8,20 @@ use Nette\Application\UI\Control;
 class BaseControl extends Control
 {
 	
+	protected function prepareTemplate()
+	{
+		$dir = dirname($this->reflection->getFileName());
+		$name = lcfirst( $this->reflection->getShortName() );
+		$file = "$dir/$name.latte";
+		$file2 = "$dir/templates/$name.latte";
+		$this->template->setFile( file_exists($file) ? $file : $file2 );
+	}
+	
+	
+	public function render()
+	{
+		$this->prepareTemplate();
+		$this->template->render();
+	}
+	
 }
--- app/presenters/BasePresenter.php
+++ app/presenters/BasePresenter.php
@@ -13,4 +13,41 @@ abstract class BasePresenter extends Presenter
 		parent::beforeRender();
 		$this->template->defaultRoute = RouterFactory::DEFAULT_ROUTE;
 	}
+	
+	
+	public function formatTemplateFiles()
+	{
+		$name = $this->getName();
+		$presenter = substr($name, strrpos(':' . $name, ':'));
+		$dir = dirname($this->getReflection()->getFileName());
+		$templateFiles = [
+			"$dir/$this->view.latte",
+			"$dir/templates/$this->view.latte",
+			"$dir/$presenter.$this->view.latte",
+			"$dir/templates/$presenter.$this->view.latte",
+		];
+		
+		$originalTemplateFiles = parent::formatTemplateFiles();
+		return array_merge($templateFiles, $originalTemplateFiles);
+	}
+	
+	
+	public function formatLayoutTemplateFiles()
+	{
+		$layoutFiles = [];
+		
+		$reflection = $this->getReflection();
+		while( $reflection->getName() !== 'Nette\Application\UI\Presenter' )
+		{
+			$dir = dirname($reflection->getFileName());
+			$layoutFiles[] = "$dir/@layout.latte";
+			$layoutFiles[] = "$dir/templates/@layout.latte";
+			
+			$reflection = $reflection->getParentClass();
+		}
+		
+		$originalLayoutFiles = parent::formatLayoutTemplateFiles();
+		return array_merge($layoutFiles, $originalLayoutFiles);
+	}
+
 }
