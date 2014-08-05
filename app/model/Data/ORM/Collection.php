640fb4e95ae00ff57cbed05049f8264d7e27e0aa
Miroslav Mrázek <miroslav.mrazek@gmail.com>
2014-08-05 04:18:18 +0200

    Opraven magický setter a přidané metody toArray() do Entity a Kolekce.

--- app/model/Data/ORM/Collection.php
+++ app/model/Data/ORM/Collection.php
@@ -7,5 +7,26 @@ use Nette\ArrayHash;
 
 class Collection extends ArrayHash
 {
+	
+	public function toArray()
+	{
+		$array = [];
+		foreach($this as $entity)
+		{
+			$array[$entity->id] = $entity->toArray();
+		}
+		return $array;
+	}
+	
+	
+	public function toArrayOfKeys()
+	{
+		$array = [];
+		foreach($this as $entity)
+		{
+			$array[] = $entity->id;
+		}
+		return $array;
+	}
 
 }
--- app/model/Data/ORM/Entity.php
+++ app/model/Data/ORM/Entity.php
@@ -2,31 +2,61 @@
 
 namespace Todolist;
 
-use LeanMapper\Entity as LeanEntity;
+use LeanMapper\Entity as LeanEntity,
+	DateTime;
 
 
 abstract class Entity extends LeanEntity
 {
 	
-/**
- * Umožňuje předat entitě místo navázaných entit jejich 'id'
- *
- * @param string $name
- * @param mixed  $value
- */
-function __set($name, $value)
-{
-	$property = $this->getReflection()->getEntityProperty($name);
-	if ($property->hasRelationship() && !($value instanceof Entity))
+	/**
+	 * Formát času, který se použije při převodu entity na pole.
+	 * @var string
+	 */
+	protected $dateTimeFormat = DateTime::ATOM;
+	
+	
+	/**
+	 * Umožňuje předat entitě místo navázaných entit jejich 'id'
+	 *
+	 * @param string $name
+	 * @param mixed  $value
+	 */
+	public function __set($name, $value)
 	{
-		$relationship = $property->getRelationship();
-		$this->row->{$property->getColumn()} = $value;
-		$this->row->cleanReferencedRowsCache($relationship->getTargetTable(), $relationship->getColumnReferencingTargetTable());
+		$property = $this->getCurrentReflection()->getEntityProperty($name);
+		if ($property->hasRelationship() && !($value instanceof Entity))
+		{
+			$relationship = $property->getRelationship();
+			$this->row->{$property->getColumn()} = $value;
+			$this->row->cleanReferencedRowsCache($relationship->getTargetTable(), $relationship->getColumnReferencingTargetTable());
+		}
+		else
+		{
+			parent::__set($name, $value);
+		}
 	}
-	else
+
+
+	public function toArray()
 	{
-		parent::__set($name, $value);
+		$array = [];
+		foreach($this->getData() as $property => $value)
+		{
+			if($value instanceof LeanEntity) {
+				$array[$property] = $value->id;
+			}
+			elseif($value instanceof Collection) {
+				$array[$property] = $value->toArrayOfKeys();
+			}
+			elseif($value instanceof DateTime) {
+				$array[$property] = $value->format($this->dateTimeFormat);
+			}
+			else {
+				$array[$property] = $value;
+			}
+		}
+		return $array;
 	}
-}
 
 }
