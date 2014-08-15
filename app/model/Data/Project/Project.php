98b21cecabab2f2eea9754f98a3d1c2048a021e6
Miroslav Mrázek <miroslav.mrazek@gmail.com>
2014-08-15 09:53:47 +0200

    Efektivní countování tasků v projektu.

--- app/model/Data/Project/Project.php
+++ app/model/Data/Project/Project.php
@@ -2,7 +2,9 @@
 
 namespace Todolist;
 
-use LeanModel\Entity;
+use LeanModel\Entity,
+	LeanMapper\Filtering,
+	LeanMapper\Fluent;
 
 
 /**
@@ -11,6 +13,8 @@ use LeanModel\Entity;
  * @property User   $user  m:hasOne
  * @property Task[] $tasks m:belongsToMany m:filter(undone)
  * 
+ * @property-read int $tasksCount m:useMethods
+ * 
  * @property int     $id
  * @property string  $title
  * @property string  $note
@@ -18,5 +22,15 @@ use LeanModel\Entity;
  */
 class Project extends Entity
 {
+	
+	public function getTasksCount()
+	{
+		$rows = $this->row->referencing('task', 'project_id', new Filtering(function (Fluent $statement) {
+			$statement->removeClause('select')->select('COUNT(*) [count], [project_id]')
+				->where(['done' => FALSE])
+				->groupBy(['project_id']);
+			}));
+		return empty($rows) ? 0 : reset($rows)->count;
+	}
 	
 }
