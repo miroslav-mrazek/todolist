d48cbfa6a7af5d2617f88260533455f5b4b9aa75
Miroslav Mrázek <miroslav.mrazek@gmail.com>
2014-10-02 04:18:28 +0200

    Nainstalován Datagrid.

--- www/js/init.js
+++ www/js/init.js
@@ -2,3 +2,9 @@ $(function () {
 	$.nette.init();
 	$.nette.ext('history').cache = false;
 });
+
+//this jQuery code for AJAX
+$(document).on('click', '.ajax:not(form)', function(e) {
+	e.preventDefault();
+	$.get($(this).attr('href'));
+});
